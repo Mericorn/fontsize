8aa5150c520b6d6bfa54bc55433a5d5a9a1cdd17
Mericorn <106124647+Mericorn@users.noreply.github.com>
2023-02-14 18:50:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+function preload(){
+
+}
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,10 +18,23 @@ function modelLoaded(){
     console.log('PoseNet Is Initialized!');
 }
 function draw() {
-background('969A97');
+background('#969A97');
+textSize(18);
+fill('#F90093');
+stroke('#F90093');
+text("Maya", 200, 300);
 }
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-    }
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+ noseX + " noseY = "+ noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX =" + leftWristX + " rightWristX = "+ rightWristX + " difference = "+ difference);
+        textSize(difference);
+        
+   }
 }
