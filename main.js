67710f3ca12c5d43d0d552c7527134f3f991707f
Mericorn <106124647+Mericorn@users.noreply.github.com>
2023-02-16 15:18:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,41 @@
-leftWrist_x = 0;
-rightWrist_x = 0;
+noseX=0;
+noseY=0;
 difference = 0;
+rightWrist_x = 0;
+leftWrist_x = 0;
+function preload(){
 
+}
 function setup(){
     video = createCapture(VIDEO);
-    video.size(400,400);
-    video.position(10,50);
-
-    canvas = createCanvas(800,400);
-    canvas.position(430,130);
-
-    poseNet = ml5.poseNet(video,modelDone);
-    poseNet.on('pose',gotposes);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150)
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
 }
-
-function draw(){
-    background("#969A97");
-    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
-    textSize(difference);
-    fill("#F90093");
-    text('Maya',200,300);
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
 }
-
-function modelDone(){
-    console.log("PoseNet Is Initialized And Loaded");
+function draw() {
+background('#969A97');
+textSize(18);
+fill('#F90093');
+stroke('#F90093');
+text("Maya", 200, 300);
 }
-
-function gotposes(results,error){
-    if(error){
-        console.error(error);
-    }
+function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+ noseX + " noseY = "+ noseY);
         leftWrist_x = results[0].pose.leftWrist.x;
         rightWrist_x = results[0].pose.rightWrist.x;
+       difference = floor(leftWrist_x - rightWrist_x);
 
-        difference = floor(leftWrist_x - rightWrist_x);
-
-        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
-        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
-    }
+        console.log("leftWristX =" + leftWrist_x + " rightWristX = "+ rightWrist_x + " difference = "+ difference);
+        textSize(difference);
+        
+   }
 }
